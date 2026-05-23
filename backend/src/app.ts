8ec6edb51c0080a4b8cbb6400b6e88c5d54097a2
yokiteshome <yordanos.ugr-0489-16@aau.edu.et>
2026-05-23 13:41:31 +0300

    Edited maximum image upload size to 10MB from the default express 100KB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,8 @@ import errorHandler from './middleware/errorMiddleware';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Routes
 app.use('/api/v1/users', userRoutes);
