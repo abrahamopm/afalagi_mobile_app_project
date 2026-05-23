3ee11c37a27db9f75030b1587c5c09ead63ed2bd
qrickybfr <manuhe.ugr-2808-16@aau.edu.et>
2026-05-23 07:25:33 +0300

    fix(desktop): initialize sqflite ffi on desktop platforms in main.dart to resolve databaseFactory not initialized exception

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'routes.dart';
 import 'core/theme/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && (Platform.isLinux || Platform.isMacOS || Platform.isWindows)) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(
     const ProviderScope(
       child: AfalagiApp(),
