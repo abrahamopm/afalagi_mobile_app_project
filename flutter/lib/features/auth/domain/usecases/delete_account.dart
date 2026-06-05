34ce9ae5972e37e11a97c59f4abdca072589edbc
abrahamopm <abraham.ugr-7532-16@aau.edu.et>
2026-06-05 17:25:23 +0300

    Delete Account test

--- flutter/lib/features/auth/domain/usecases/delete_account.dart
+++ flutter/lib/features/auth/domain/usecases/delete_account.dart
@@ -1,4 +1,4 @@
-import 'package:afalagi/Core/usecases/usecase.dart';
+import 'package:afalagi/core/usecases/usecase.dart';
 import '../repositories/auth_repository.dart';
 
 class DeleteAccount extends UseCase<void, NoParams> {
